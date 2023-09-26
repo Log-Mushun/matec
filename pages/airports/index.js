41ce968184a5171a4728d5a42a77dcfff33fe262
Michelle <cadavidcarmona1@gmail.com>
2023-09-25 20:09:54 -0500

    final

--- pages/airports/index.js
+++ pages/airports/index.js
@@ -3,12 +3,10 @@ import LogoDarkComp from '../../components/Airports/LogoDarkComp'
 import BurgerMenu from '../../components/Airports/BurgerMenu'
 import AirportsComp from '../../components/Airports/AirportsComp'
 import DriverOne from '../../components/Airports/DriverOne'
-import WebGiViewer from '../../components/Airports/WebGiViewer'
 
 export default function Airports() {
   return (
     <> 
-    <WebGiViewer/>
       <div className="flex flex-row">
         <LogoDarkComp/>
         <BurgerMenu/>
