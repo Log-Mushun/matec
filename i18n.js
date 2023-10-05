f1d912e7860e76551f164570449265a7d732d8d9
Michelle <cadavidcarmona1@gmail.com>
2023-10-05 14:58:35 -0500

    ome ome que cosas

--- i18n.js
+++ i18n.js
@@ -198,7 +198,6 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en',
     fallbackLng: 'en',
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
