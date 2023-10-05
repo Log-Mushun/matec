c6dc6ebeef88e11cf2dbed419f2f97cb262c7068
Michelle <cadavidcarmona1@gmail.com>
2023-10-05 15:17:21 -0500

    next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
+const runtimeCaching = require('next-pwa/cache')
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: process.env.NODE_ENV === 'development',
+  buildExcludes: [/middleware-manifest.json$/],
+  maximumFileSizeToCacheInBytes: 4000000,
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true, transpilePackages: ['gsap'],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }
 }
 
-module.exports = nextConfig
+module.exports = () => {
+  const plugins = [withPWA]
+  return plugins.reduce((acc, plugin) => plugin(acc), {
+    ...nextConfig,
+  })
+}
