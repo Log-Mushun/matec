9dfcea61262eb25b8766b99dffb35cc9436e1100
Michelle <cadavidcarmona1@gmail.com>
2023-10-05 17:22:30 -0500

    font size

--- components/AirportsFooter.jsx
+++ components/AirportsFooter.jsx
@@ -8,16 +8,16 @@ const AirportsFooter = () => {
     return (
       <I18nextProvider i18n={i18nfile}>
        <div className="bg-transparent flex flex-col z-30 experience_footer fixed bottom-1 " style={{zIndex:1000}}>
-  <div className="flex flex-row justify-between pl-14 pb-5 align-center">
-    <div className="flex flex-row align-center justify-center">
+  <div className="flex flex-row justify-between pl-14 pb-5 items-end">
+    <div className="flex flex-row items-end">
       <h3 className="row_text_small">{t("footer-credits")}</h3>
       <h3 className="row_text_small">{t("footer-privacy")}</h3>
       <h3 className="row_text_small">{t("footer-cookies")}</h3>
-      <Languages />
     </div>
-    <div className="flex flex-row pr-12 fixed right-5">
+    <div className="flex flex-row pr-12 fixed right-5 items-end">
       <h2 className="row_text_big">Facebook</h2>
-      <h2 className="row_text_big">Instagram</h2>
+      <h2 className="row_text_big pr-4">Instagram</h2>
+      <Languages />
     </div>
   </div>
 </div>
