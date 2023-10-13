f6e0426f9e5071f257a5a4c7a046d8cafd25e743
Michelle <cadavidcarmona1@gmail.com>
2023-10-13 18:29:36 -0500

    main page

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,12 @@ import Link from 'next/link';
 import { FaSuitcaseRolling } from 'react-icons/fa'
 import { GiCardboardBoxClosed } from 'react-icons/gi'
 import { FaBoxesPacking } from 'react-icons/fa6'
+import { TbBoxSeam } from 'react-icons/tb'
+import { TbPlane } from 'react-icons/tb'
+import { TbTruckDelivery } from 'react-icons/tb'
+
 import { I18nextProvider, useTranslation } from "react-i18next";
-import {i18n as i18nfile} from "../i18n";
+import { i18n as i18nfile } from "../i18n";
 
 // framer motion
 import { AnimatePresence, motion } from 'framer-motion';
@@ -67,84 +71,90 @@ const Home = () => {
   return (
     <I18nextProvider i18n={i18nfile}>
 
-    <AnimatePresence>
-      <motion.div className='h-full'>
-        <Transition />
-        <div className='container'>
-          <HeaderSelector />
-          <div className='h-screen w-screen bg-gradient-radial-c flex flex-col justify-center items-center'>
-            {/* text */}
-            <div className='relative top-[15%] pb-5 md:pb-10 lg:pb-0 z-10'>
-              <h4
-                id='titulo_selector'
-                className='text-l md:text-2xl lg:text-2xl text-[#003b71] opacity-80 text-center hover:opacity-20 transition-opacity duration-300 z-10'
-              >
-                {t('selector-title')}
-              </h4>
+      <AnimatePresence>
+        <motion.div className='h-full'>
+          <Transition />
+          <div className='container'>
+            <HeaderSelector />
+            <div className='h-screen w-screen bg-gradient-radial-c flex flex-col justify-center items-center'>
+              {/* text */}
+              <div className='relative top-[15%] pb-5 md:pb-10 lg:pb-0 z-10'>
+                <h4
+                  id='titulo_selector'
+                  className='text-l md:text-2xl lg:text-2xl text-[#003b71] opacity-80 text-center hover:opacity-20 transition-opacity duration-300 z-10'
+                >
+                  {t('selector-title')}
+                </h4>
 
+
+              </div>
+              <div className='text-center md:flex-row lg:flex-row flex items-center justify-center sm:gap-0 lg:gap-20 lg:pt-20  container mx-auto flex-col relative top-[10%]'>
+                {/*suitcase*/}
+                <motion.div
+                  className='z-10 flex items-center mb-8'
+                  variants={fadeIn('down', 0.2)}
+                  initial='hidden'
+                  animate='show'
+                  exit='hidden'
+                >
+                  <Link href='/airports'>
+                    <div className='bg-transparent h-full w-[12rem] flex justify-end flex-col items-center'>
+                      <div className='hover:bg-hoverbg hover:bg-cover hover:bg-no-repeat'>
+                        <TbPlane size={128} className='text-[#003d71] lg:w-36 lg:h-36 md:w-20 md:h-20 w-14 z-10 hidden md:inline' />
+                      </div>
+                      <h1 className='text-[#003d71] text-center pt-5 z-10 text-2xl'>{t('selector-aeropuerto')}</h1>
+                    </div>
+                  </Link>
+                </motion.div>
+                {/* papers */}
+                <motion.div
+                  className='z-10 flex items-center mb-8'
+                  variants={fadeIn('down', 0.4)}
+                  initial='hidden'
+                  animate='show'
+                  exit='hidden'
+                >                <Link href='/airports'>
+                    <div className='bg-transparent h-full w-[12rem] flex justify-end flex-col items-center'>
+                      <div className='hover:bg-hoverbg hover:bg-cover hover:bg-no-repeat'>
+                        <TbBoxSeam size={128} className='text-[#003d71] lg:w-36 lg:h-36 md:w-20 md:h-20 hidden lg:block md:inline' />
+                      </div>
+                      <h1 className='text-[#003d71] text-center pt-5 text-2xl'>{t('selector-papel')}</h1>
+                    </div>
+                  </Link>
+                </motion.div>
+                {/* logistics */}
+                <motion.div
+                  className='z-10 flex items-center mb-8'
+                  variants={fadeIn('down', 0.4)}
+                  initial='hidden'
+                  animate='show'
+                  exit='hidden'
+                >
+                  <Link href='/airports'>
+                    <div className='bg-transparent h-full w-[12rem] flex justify-end flex-col items-center'>
+                      <div className='hover:bg-hoverbg hover:bg-cover hover:bg-no-repeat'>
+                        <TbTruckDelivery size={128} className='text-[#003d71] lg:w-36 lg:h-36 md:w-20 md:h-20 hidden lg:block md:inline' />
+                      </div>
+                      <h1 className='text-[#003d71] text-center pt-5 text-2xl'>{t('selector-logi')}</h1>
+                    </div>
+                  </Link>
+                </motion.div>
+              </div>
+              <div className='fixed bottom-0 left-0 w-full p-4 text-center z-10'>
+                <div className='flex justify-center items-center'>
+                  <Languages />
+                </div>
+              </div>
+            </div>
+
+            <div className='w-full h-full absolute right-0 bottom-0 z-0'>
+              <Plexus isStart={true} />
 
             </div>
-            <div className='text-center md:flex-row lg:flex-row flex items-center justify-center sm:gap-0 lg:gap-20 lg:pt-20  container mx-auto flex-col relative top-[10%]'>
-              {/*suitcase*/}
-              <motion.div
-                className='z-10 flex items-center mb-8'
-                variants={fadeIn('down', 0.2)}
-                initial='hidden'
-                animate='show'
-                exit='hidden'
-              >
-                <Link href='/airports'>
-                  <div className='bg-transparent hover:bg-hoverbg hover:bg-cover hover:bg-no-repeat h-full w-[12rem] flex justify-end flex-col items-center'>
-                    <FaSuitcaseRolling size={128} className='text-[#003d71] lg:w-36 lg:h-36 md:w-20 md:h-20 w-14 z-10 hidden md:inline' />
-                    <h1 className='text-[#003d71] text-center pt-5 z-10 text-2xl'>{t('selector-aeropuerto')}</h1>
-                  </div>
-                </Link>
-              </motion.div>
-              {/* papers */}
-              <motion.div
-                className='z-10 flex items-center mb-8'
-                variants={fadeIn('down', 0.4)}
-                initial='hidden'
-                animate='show'
-                exit='hidden'
-              >                <Link href='/airports'>
-                  <div className='bg-transparent hover:bg-hoverbg hover:bg-cover hover:bg-no-repeat h-full w-[12rem] flex justify-end flex-col items-center'>
-                    <GiCardboardBoxClosed size={128} className='text-[#003d71] lg:w-36 lg:h-36 md:w-20 md:h-20 hidden lg:block md:inline' />
-                    <h1 className='text-[#003d71] text-center pt-5 text-2xl'>{t('selector-papel')}</h1>
-                  </div>
-                </Link>
-              </motion.div>
-              {/* logistics */}
-              <motion.div
-                className='z-10 flex items-center mb-8'
-                variants={fadeIn('down', 0.4)}
-                initial='hidden'
-                animate='show'
-                exit='hidden'
-              >
-                <Link href='/airports'>
-                  <div className='bg-transparent hover:bg-hoverbg hover:bg-cover hover:bg-no-repeat h-full w-[12rem] flex justify-end flex-col items-center'>
-                    <FaBoxesPacking size={128} className='text-[#003d71] lg:w-36 lg:h-36 md:w-20 md:h-20 hidden lg:block md:inline' />
-                    <h1 className='text-[#003d71] text-center pt-5 text-2xl'>{t('selector-logi')}</h1>
-                  </div>
-                </Link>
-              </motion.div>
-            </div>
-            <div className='fixed bottom-0 left-0 w-full p-4 text-center z-10'>
-        <div className='flex justify-center items-center'>
-          <Languages />
-        </div>
-      </div>
-          </div>
-          
-          <div className='w-full h-full absolute right-0 bottom-0 z-0'>
-            <Plexus isStart={true} />
 
           </div>
-
-        </div>
-      </motion.div>
-    </AnimatePresence>
+        </motion.div>
+      </AnimatePresence>
     </I18nextProvider>
 
   );
